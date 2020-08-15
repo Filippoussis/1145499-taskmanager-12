cbf04dacb26bd194460f8179734cca06a1a3a0ce
Filippoussis <filippoussis@iMac-AndrosPhilippos.local>
2020-08-15 19:18:16 +0500

    Переименовал button

--- src/view/button.js
+++
@@ -1,9 +0,0 @@
-/**
- * создает шаблон кнопки
- * @return {string} возвращает HTML-строку в ввиде кода HTML
- */
-export const createLoadMoreButtonTemplate = () => {
-  return (
-    `<button class="load-more" type="button">load more</button>`
-  );
-};
